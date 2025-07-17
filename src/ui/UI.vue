5c054b5879b5bb48de287f854a76dd6165b1a603
narvita <narkha21@gmail.com>
2025-07-17 17:59:36 +0400

    created bar menu ui

--- src/ui/UI.vue
+++ src/ui/UI.vue
@@ -1,87 +1,414 @@
 <!-- src/components/UI.vue -->
 <template>
-    <div class="ui-overlay">
-        <span>Balance: </span><span id='balance' class='balance-amount'>{{ balance }} $</span>
-        <button class='spin-button' @click="spinButtonClick">Spin</button>
-        <button class='minus-button bet-button' @click="minusButtonClick">-</button>
-        <span>Bet Amount: </span><span id='bet' class='bet-amount'>{{ DEFAULT_BET }} $</span>
-        <button class='plus-button bet-button' @click="plusButtonClick">+</button>
+  <div class="menu-wrapper">
+    <div class="wrapper">
+      <svg
+        class="menu-background"
+        width="1386"
+        viewBox="0 0 1386 193"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <foreignObject x="0.10714" y="0.317141" width="1385.79" height="213.786"
+          ><div
+            xmlns="http://www.w3.org/1999/xhtml"
+            style="
+              backdrop-filter: blur(9.87px);
+              clip-path: url(#bgblur_0_77_4844_clip_path);
+              height: 100%;
+              width: 100%;
+            "
+          ></div
+        ></foreignObject>
+        <g filter="url(#filter0_d_77_4844)" data-figma-bg-blur-radius="19.7357">
+          <mask id="path-1-inside-1_77_4844" fill="white">
+            <path
+              d="M686.501 24C709.984 24 730.911 34.9399 744.468 52H1307C1332.41 52 1353 72.5949 1353 98C1353 123.405 1332.41 144 1307 144H744.468C730.911 161.06 709.984 172 686.501 172C663.018 172 642.09 161.06 628.534 144H79C53.5949 144 33 123.405 33 98C33 72.5949 53.5949 52 79 52H628.534C642.09 34.9399 663.018 24 686.501 24Z"
+            />
+          </mask>
+          <path
+            d="M686.501 24C709.984 24 730.911 34.9399 744.468 52H1307C1332.41 52 1353 72.5949 1353 98C1353 123.405 1332.41 144 1307 144H744.468C730.911 161.06 709.984 172 686.501 172C663.018 172 642.09 161.06 628.534 144H79C53.5949 144 33 123.405 33 98C33 72.5949 53.5949 52 79 52H628.534C642.09 34.9399 663.018 24 686.501 24Z"
+            fill="white"
+            fill-opacity="0.25"
+            shape-rendering="crispEdges"
+          />
+          <path
+            d="M744.468 52L743.438 52.8185L743.833 53.3157H744.468V52ZM1307 52V50.6843V52ZM1307 144V145.316V144ZM744.468 144V142.684H743.833L743.438 143.181L744.468 144ZM628.534 144L629.564 143.181L629.169 142.684H628.534V144ZM79 144V145.316V144ZM79 52V50.6843V52ZM628.534 52V53.3157H629.169L629.564 52.8185L628.534 52ZM686.501 24V25.3157C709.566 25.3157 730.12 36.059 743.438 52.8185L744.468 52L745.498 51.1815C731.703 33.8208 710.403 22.6843 686.501 22.6843V24ZM744.468 52V53.3157H1307V52V50.6843H744.468V52ZM1307 52V53.3157C1331.68 53.3157 1351.68 73.3216 1351.68 98H1353H1354.32C1354.32 71.8683 1333.13 50.6843 1307 50.6843V52ZM1353 98H1351.68C1351.68 122.678 1331.68 142.684 1307 142.684V144V145.316C1333.13 145.316 1354.32 124.132 1354.32 98H1353ZM1307 144V142.684H744.468V144V145.316H1307V144ZM744.468 144L743.438 143.181C730.12 159.941 709.566 170.684 686.501 170.684V172V173.316C710.403 173.316 731.703 162.179 745.498 144.819L744.468 144ZM686.501 172V170.684C663.436 170.684 642.882 159.941 629.564 143.181L628.534 144L627.504 144.819C641.299 162.179 662.599 173.316 686.501 173.316V172ZM628.534 144V142.684H79V144V145.316H628.534V144ZM79 144V142.684C54.3216 142.684 34.3157 122.678 34.3157 98H33H31.6843C31.6843 124.132 52.8683 145.316 79 145.316V144ZM33 98H34.3157C34.3157 73.3216 54.3216 53.3157 79 53.3157V52V50.6843C52.8683 50.6843 31.6843 71.8683 31.6843 98H33ZM79 52V53.3157H628.534V52V50.6843H79V52ZM628.534 52L629.564 52.8185C642.882 36.059 663.436 25.3157 686.501 25.3157V24V22.6843C662.599 22.6843 641.299 33.8208 627.504 51.1815L628.534 52Z"
+            fill="white"
+            fill-opacity="0.5"
+            mask="url(#path-1-inside-1_77_4844)"
+          />
+        </g>
+        <defs>
+          <filter
+            id="filter0_d_77_4844"
+            x="0.10714"
+            y="0.317141"
+            width="1385.79"
+            height="213.786"
+            filterUnits="userSpaceOnUse"
+            color-interpolation-filters="sRGB"
+          >
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dy="9.21" />
+            <feGaussianBlur stdDeviation="16.4464" />
+            <feComposite in2="hardAlpha" operator="out" />
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.5 0"
+            />
+            <feBlend
+              mode="normal"
+              in2="BackgroundImageFix"
+              result="effect1_dropShadow_77_4844"
+            />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="effect1_dropShadow_77_4844"
+              result="shape"
+            />
+          </filter>
+          <clipPath
+            id="bgblur_0_77_4844_clip_path"
+            transform="translate(-0.10714 -0.317141)"
+          >
+            <path
+              d="M686.501 24C709.984 24 730.911 34.9399 744.468 52H1307C1332.41 52 1353 72.5949 1353 98C1353 123.405 1332.41 144 1307 144H744.468C730.911 161.06 709.984 172 686.501 172C663.018 172 642.09 161.06 628.534 144H79C53.5949 144 33 123.405 33 98C33 72.5949 53.5949 52 79 52H628.534C642.09 34.9399 663.018 24 686.501 24Z"
+            />
+          </clipPath>
+        </defs>
+      </svg>
+      <div class="ui-overlay">
+        <div class="section left-section">
+          <div class="close-button-wrapper">
+            <div class="close-button">
+              <img src="/src/assets/images/close.svg" />
+            </div>
+            <div class="line"></div>
+          </div>
+          <div class="balance-wrapper full-width">
+            <span class="text">Balance: </span>
+            <span id="balance" class="amount"> $ {{ balance }}</span>
+          </div>
+        </div>
+
+        <div class="section middle-section">
+          <div class="button-box">
+            <button class="spin-button" @click="spinButtonClick">
+              <img src="../assets/images/spin.svg" />
+              <div class="dot"></div>
+            </button>
+          </div>
+        </div>
+
+        <div class="section right-section">
+          <div class="refresh-btn-wrapper">
+            <div class="refresh-btn">
+              <img src="../assets/images/refresh.svg" />
+            </div>
+            <div class="line"></div>
+          </div>
+
+          <div class="balance-box">
+            <div class="balance-wrapper">
+              <span class="text">Bet </span
+              ><span id="bet" class="amount">${{ DEFAULT_BET }} </span>
+            </div>
+            <div class="bet-button-wrapper">
+              <button
+                class="increase-button bet-button"
+                @click="plusButtonClick"
+              >
+                <img src="../assets/images/arrow.svg" />
+              </button>
+              <button
+                class="decrease-button bet-button"
+                @click="minusButtonClick"
+              >
+                <img src="../assets/images/arrow.svg" />
+              </button>
+            </div>
+          </div>
+
+          <div class="bonuse-btn-wrapper">
+            <div class="line"></div>
+            <div class="refresh-btn">
+                <span class="buy-btn-text">
+                    Buy Bonus
+                </span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-
-import { lego } from '@armathai/lego';
-import { DEFAULT_BET } from '../configs/SymbolsConfig';
-import { SlotMachineViewEvents, UIEvents } from '../events/MainEvents';
-import { PlayerModelEvents } from '../events/ModelEvents';
+import { lego } from "@armathai/lego";
+import { DEFAULT_BET } from "../configs/SymbolsConfig";
+import { SlotMachineViewEvents, UIEvents } from "../events/MainEvents";
+import { PlayerModelEvents } from "../events/ModelEvents";
 
 let tempBalance = -1;
-let balance = -1
+let balance = -1;
 
-const spinButtonClick = () => lego.event.emit(UIEvents.SpinButtonClick)
+const spinButtonClick = () => lego.event.emit(UIEvents.SpinButtonClick);
 const plusButtonClick = () => lego.event.emit(UIEvents.PlusButtonClick);
 const minusButtonClick = () => lego.event.emit(UIEvents.MinusButtonClick);
 
 const updateTempBalance = (newBalance: number): void => {
-    if (tempBalance === -1) {
-        tempBalance = newBalance;
-        const betElement = document.getElementById('balance');
+  if (tempBalance === -1) {
+    tempBalance = newBalance;
+    const betElement = document.getElementById("balance");
 
-        if (betElement) {
-            betElement.textContent = `${tempBalance} $`;
-        }
-    } else {
-        tempBalance = newBalance;
+    if (betElement) {
+      betElement.textContent = `${tempBalance} $`;
     }
-
-}
+  } else {
+    tempBalance = newBalance;
+  }
+};
 
 const updateBalance = (): void => {
-    const betElement = document.getElementById('balance');
+  const betElement = document.getElementById("balance");
 
-    if (betElement) {
-        betElement.textContent = `${tempBalance} $`;
-    }
-}
+  if (betElement) {
+    betElement.textContent = `${tempBalance} $`;
+  }
+};
 
 const betUpdate = (bet: number) => {
-    const betElement = document.getElementById('bet');
-    if (betElement) {
-        betElement.textContent = `${bet} $`;
-    }
-}
+  const betElement = document.getElementById("bet");
+  if (betElement) {
+    betElement.textContent = `${bet} $`;
+  }
+};
 
-
-lego.event.on(PlayerModelEvents.BetUpdate, betUpdate)
+lego.event.on(PlayerModelEvents.BetUpdate, betUpdate);
 lego.event.on(PlayerModelEvents.BalanceUpdate, updateTempBalance);
 lego.event.on(SlotMachineViewEvents.WinningsShowComplete, updateBalance);
-
 </script>
 
 <style scoped>
+.line {
+  width: 1px;
+  height: 60px;
+  background: linear-gradient(
+    to bottom,
+    rgba(192, 177, 165, 0) 0%,
+    rgba(255, 255, 255, 1) 50%,
+    rgba(153, 153, 153, 0) 100%
+  );
+}
+
+.balance-box {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+
+.full-width {
+    width: 100%;
+}
+
+.balance-wrapper {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+}
+
+.section {
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+}
+
+.middle-section {
+  max-width: fit-content;
+}
+
+.left-section {
+  justify-content: start;
+  padding-left: 3%;
+}
+
+.right-section {
+  justify-content: end;
+  padding-right: 3%;
+}
+.text {
+  font-size: 16px;
+  color: rgba(255, 255, 255, 0.7);
+}
+
+.menu-wrapper {
+  background-size: cover;
+  position: absolute;
+  bottom: -2%;
+  width: 65%;
+  display: flex;
+  align-self: center;
+  left: 17.5%;
+}
+
+.wrapper {
+  position: relative;
+  height: auto;
+  width: 100%;
+}
+
+.menu-background {
+  width: 100%;
+  height: auto;
+}
+
 .ui-overlay {
-    position: absolute;
-    bottom: 10px;
-    left: 40%;
-    z-index: 10;
-    background-color: aqua;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 10px;
+  border-radius: 100px;
+}
+
+.close-button-wrapper,
+.refresh-btn-wrapper, 
+.bonuse-btn-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.close-button,
+.refresh-btn {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 60px;
+  height: 60px;
+  margin: 10px;
+}
+
+.close-button img,
+.refresh-btn img {
+  width: 30px;
+  height: 30px;
+}
+
+.buy-btn-text {
+    color: rgba(0, 113, 30, 1);
+    font-size: 13px;
+    text-align: center;
+    word-wrap: break-word;
+    font-weight: 900;
+}
+.button-box {
+  transform: translateX(-4.3%);
+  border-radius: 50%;
+  border: 1.3px solid transparent;
+  background-image:
+    linear-gradient(rgba(255, 255, 255, 0.25), rgba(255, 255, 255, 0.25)),
+    linear-gradient(
+      135deg,
+      rgba(255, 255, 255, 0.25),
+      rgba(153, 153, 153, 0.25)
+    );
+
+  background-origin: border-box;
+  background-clip: content-box, border-box;
+
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+  left: 50%;
 }
 
 .spin-button {
-    background-color: #04AA6D;
-    border: none;
-    color: white;
-    padding: 15px 32px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 16px;
+  background-color: rgba(255, 255, 255, 0.25);
+  padding: 0;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  width: 80px;
+  height: 80px;
+  background: radial-gradient(
+    circle at 40% 35%,
+    rgba(255, 255, 255, 0.5),
+    rgba(168, 147, 121, 0.5)
+  );
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 50%;
+  border: none;
+  color: white;
+  text-align: center;
+  text-decoration: none;
+  font-size: 16px;
+}
+
+.spin-button img {
+  width: 60px;
+  height: 60px;
+}
+
+.dot {
+  width: 14px;
+  height: 14px;
+  background-color: #00711e;
+  border-radius: 50%;
+  box-shadow: 2px 2px 2px 0px rgba(255, 255, 255, 0.4) inset;
+}
+
+.amount {
+  margin-left: 10px;
+  margin-right: 10px;
+  color: rgba(255, 255, 255, 1);
+  font-size: 20px;
+}
+
+.refresh-btn {
 }
 
 .bet-button,
-.balance-amount {
-    margin-left: 10px;
-    margin-right: 10px;
+.close-button,
+.refresh-btn {
+  border-radius: 50%;
+  background: radial-gradient(
+    at left top,
+    rgba(255, 255, 255, 0.55),
+    rgba(168, 147, 121, 0.55)
+  );
+  border: none;
+}
+
+.bet-button-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-left: 10px;
+}
+
+.decrease-button,
+.increase-button {
+  width: 28px;
+  height: 28px;
+  padding: 0;
+}
+
+.increase-button img {
+  rotate: 180deg;
 }
 </style>
