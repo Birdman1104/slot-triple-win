d12613296b11964d91f0bc0d3a42d883ed42c2ee
narvita <narkha21@gmail.com>
2025-08-29 22:39:41 +0400

    refactored the ui code

--- src/ui/Modal.vue
+++ src/ui/Modal.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="modal-overlay" @click.stop>
-    <div class="modal-content" :class="['modal-content', props.customClass]" :style="{ width: props.width + 'px' }">
-      <img class="close-btn" v-if="isMobile && props.modal === 'menu'" src="/src/assets/icons/close.svg" />
+    <div
+      class="modal-content"
+      :class="['modal-content', props.customClass]"
+      :style="{ width: props.width + 'px' }"
+    >
+      <img
+        class="close-btn"
+        v-if="isMobile && props.modal === 'menu'"
+        src="/src/assets/icons/close.svg"
+        @pointerdown="handleClose"
+      />
       <ul>
         <li
           v-for="item in items"
@@ -56,7 +65,6 @@ const props = defineProps({
 const emits = defineEmits(["close", "select"]);
 
 function selectItem(item) {
-  
   selectedItemId.value = item.id;
   emits("select", item);
 }
@@ -66,7 +74,7 @@ function handleResize() {
 }
 
 function handleClose() {
-  isMobile.value = window.innerWidth <= 768;
+  emits("close");
 }
 
 onMounted(() => {
@@ -146,9 +154,11 @@ li {
   width: 30px;
   height: 30px;
   border-radius: 50%;
-  background: radial-gradient(circle at 35% 40%,
-      rgba(255, 255, 255, 0.26),
-      rgba(168, 147, 121, 0.26));
+  background: radial-gradient(
+    circle at 35% 40%,
+    rgba(255, 255, 255, 0.26),
+    rgba(168, 147, 121, 0.26)
+  );
   box-shadow: 0 5px 15px rgba(0, 0, 0, 0.1);
   border: 1px;
   backdrop-filter: blur(10px);
@@ -164,7 +174,8 @@ li {
   border-radius: 50%;
   background:
     linear-gradient(to right, #00be32, #00711e) border-box,
-    radial-gradient(circle, rgba(0, 113, 30, 1), rgba(0, 142, 38, 1)) padding-box;
+    radial-gradient(circle, rgba(0, 113, 30, 1), rgba(0, 142, 38, 1))
+      padding-box;
   background-clip: content-box, border-box;
 }
 
@@ -186,7 +197,7 @@ li {
 <style>
 @media screen and (min-width: 1500px) {
   .modal-content {
-    bottom: 120% !important;
+    bottom: 90% !important;
   }
 }
 </style>
