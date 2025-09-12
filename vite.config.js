d365210688b4c7aca856e5f93856b82ba13daa09
Birdman1104 <arsenmazmanyan11@gmail.com>
2025-09-12 23:47:17 +0400

    Added vue dev tools.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueDevTools()],
   base: process.env.NODE_ENV === "production" ? "/slot-triple-win/" : "",
 
   build: {
