b258bc1254f1a5e5554393b5b8e4582f873bea71
Birdman1104 <arsenmazmanyan11@gmail.com>
2025-09-22 17:15:03 +0400

    removed vue.

--- src/ui/components/menu/UI.vue
+++
@@ -1,5 +0,0 @@
-<script src="./menu.script.ts" lang="ts"></script>
-<style src="./menu.styles.css" scoped></style>
-<template src="./menu.template.html">
-
-</template>
--- src/ui/enums/ui-enums.ts
+++
@@ -1,7 +0,0 @@
-export enum MenuEnum {
-  History = "History",
-  Turbo = "Turbo",
-  Info = "Info",
-  Music = "Music",
-  Sound = "Sound",
-}
