9f271901d479a88e7fc0ec93c976f857c2021e83
Birdman1104 <arsenmazmanyan11@gmail.com>
2025-09-30 17:55:37 +0400

    Added leafs and shadows for intro view.

--- src/configs/gridConfigs/backgroundViewGC.ts
+++ src/configs/gridConfigs/backgroundViewGC.ts
@@ -1,4 +1,4 @@
-import { CellScale } from "@armathai/pixi-grid";
+import { CellAlign, CellScale } from "@armathai/pixi-grid";
 import { lp } from "../../utils/Utils";
 
 export const getBackgroundGridConfig = () => {
@@ -22,6 +22,24 @@ const getBackgroundGridLandscapeConfig = () => {
         scale: CellScale.envelop,
         bounds: { x: 0, y: 0, width: 1, height: 1 },
       },
+      {
+        name: "leaf",
+        bounds: { x: 0.7, y: 0, width: 0.3, height: 0.3 },
+        align: CellAlign.rightTop,
+        scale: CellScale.envelop,
+      },
+      {
+        name: "left_shadow",
+        bounds: { x: 0, y: 0.75, width: 0.3, height: 0.25 },
+        align: CellAlign.leftBottom,
+        scale: CellScale.envelop,
+      },
+      {
+        name: "right_shadow",
+        bounds: { x: 0.7, y: 0.75, width: 0.3, height: 0.25 },
+        align: CellAlign.rightBottom,
+        scale: CellScale.envelop,
+      },
     ],
   };
 };
@@ -42,6 +60,24 @@ const getBackgroundGridPortraitConfig = () => {
         name: "bkg",
         scale: CellScale.envelop,
         bounds: { x: 0, y: 0, width: 1, height: 1 },
+      },
+      {
+        name: "leaf",
+        bounds: { x: 0.5, y: 0, width: 0.5, height: 0.3 },
+        align: CellAlign.rightTop,
+        scale: CellScale.showAll,
+      },
+      {
+        name: "left_shadow",
+        bounds: { x: 0, y: 0.7, width: 0.5, height: 0.3 },
+        align: CellAlign.leftBottom,
+        scale: CellScale.showAll,
+      },
+      {
+        name: "right_shadow",
+        bounds: { x: 0.5, y: 0.7, width: 0.5, height: 0.3 },
+        align: CellAlign.rightBottom,
+        scale: CellScale.showAll,
       },
     ],
   };
--- src/views/BackgroundView.ts
+++ src/views/BackgroundView.ts
@@ -1,11 +1,20 @@
+import { lego } from "@armathai/lego";
 import { PixiGrid, type ICellConfig } from "@armathai/pixi-grid";
+import type { Sprite } from "pixi.js";
 import { getBackgroundGridConfig } from "../configs/gridConfigs/backgroundViewGC";
-import { gameBkgSpriteConfig } from "../configs/spritesConfig";
+import { dockLeftShadowConfig, dockRightShadowConfig, gameBkgSpriteConfig, leafConfig } from "../configs/spritesConfig";
+import { GameModelEvents } from "../events/ModelEvents";
+import { GameState } from "../models/GameModel";
 import { makeSprite } from "../utils/Utils";
 
 export class BackgroundView extends PixiGrid {
+  private leftShadow!: Sprite;
+  private rightShadow!: Sprite;
+  private leaf!: Sprite;
   constructor() {
     super();
+
+    lego.event.on(GameModelEvents.StateUpdate, this.onStateUpdate, this);
     this.build();
   }
 
@@ -17,8 +26,23 @@ export class BackgroundView extends PixiGrid {
     super.rebuild(this.getGridConfig());
   }
 
+  private onStateUpdate(state: GameState): void {
+    if (state === GameState.Game) {
+      this.leftShadow.destroy();
+      this.rightShadow.destroy();
+      this.leaf.destroy();
+    }
+  }
+
   private build(): void {
     const bkg = makeSprite(gameBkgSpriteConfig());
     this.setChild("bkg", bkg);
+
+    this.leftShadow = makeSprite(dockLeftShadowConfig());
+    this.setChild("left_shadow", this.leftShadow);
+    this.rightShadow = makeSprite(dockRightShadowConfig());
+    this.setChild("right_shadow", this.rightShadow);
+    this.leaf = makeSprite(leafConfig());
+    this.setChild("leaf", this.leaf);
   }
 }
